ce366e7f853b48a598d6a554c795ea5e2a3c8aa8
User <user@kpito-bsu-pc16.ag.co.id>
2020-01-02 17:33:13 +0700

    map and slice

--- map.go
+++ map.go
@@ -7,18 +7,18 @@ import (
 
 func main() {
 	bulan := map[int]string{
-		1 : "Januari",
-		2 : "Februari",
-		3 : "Maret",
-		4 : "April",
-		5 : "Mei",
-		6 : "Juni",
-		7 : "Juli",
-		8 : "Agustus",
-		9 : "September",
-		10 : "Oktober",
-		11 : "November",
-		12 : "Desember",
+		1:  "Januari",
+		2:  "Februari",
+		3:  "Maret",
+		4:  "April",
+		5:  "Mei",
+		6:  "Juni",
+		7:  "Juli",
+		8:  "Agustus",
+		9:  "September",
+		10: "Oktober",
+		11: "November",
+		12: "Desember",
 	}
 
 	//var keys []int
@@ -38,7 +38,7 @@ func main() {
 	}
 	fmt.Println("-------------")
 	fmt.Println("Default Order Map (asc)")
-	for _, k := range sortedKeysAsc(bulan){
+	for _, k := range sortedKeysAsc(bulan) {
 		fmt.Println("Bulan ke ", k, "adalah", bulan[k])
 	}
 	fmt.Println("-------------")
@@ -49,7 +49,34 @@ func main() {
 		fmt.Println("Not Exist")
 	}
 
-	fmt.Println()
+	fmt.Println("------------------")
+	var chickens = []map[string]string{
+		{"name": "chicken blue", "gender": "male"},
+		{"name": "chicken red", "gender": "male"},
+		{"name": "chicken yellow", "gender": "female"},
+	}
+
+	for _, chicken := range chickens {
+		fmt.Println(chicken["gender"], chicken["name"])
+	}
+
+	fmt.Println("----------------")
+	var data = []map[string]string{
+		{"nama": "Ryan", "gender": "male", "skin": "yellow"},
+		{"address": "Jakarta", "id": "201"},
+		{"hobbies": "movie"},
+	}
+
+	sliceKeys := make([]int, cap(data))
+	//sKeys := make([]string, cap(data))
+	i := 0
+	for k := range data {
+		sliceKeys[i] = k
+		fmt.Println(sliceKeys[i])
+		i++
+	}
+
+	fmt.Println(data[0]["nama"])
 }
 
 type reversedKeys struct {
@@ -57,7 +84,7 @@ type reversedKeys struct {
 	Value string
 }
 
-func sortedKeysAsc(m map[int]string) ([]int) {
+func sortedKeysAsc(m map[int]string) []int {
 	keys := make([]int, len(m))
 	i := 0
 	for k := range m {
@@ -67,5 +94,3 @@ func sortedKeysAsc(m map[int]string) ([]int) {
 	sort.Ints(keys)
 	return keys
 }
-
-
